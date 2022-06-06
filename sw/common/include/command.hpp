cd37afb6480fc0da6c5f00547a2a67c5f211cf6a
Brian Wiec <bswiec@gmail.com>
2022-06-06 16:58:44 -0400

    Filled out command with all fields

--- sw/common/include/command.hpp
+++ sw/common/include/command.hpp
@@ -9,17 +9,48 @@ using namespace std;
 #define NUM_CHANNELS 4
 #endif
 
+typedef enum pattern
+{
+  PATTERN_SINE = 0,
+  PATTERN_SQUARE = 1,
+  PATTERN_TRIANGLE = 2,
+  PATTERN_SAWTOOTH = 3,
+  NUM_PATTERNS
+} pattern_t;
+
 class command
 {
   private:
-    unsigned char channel_is_enabled;
+    unsigned char _channel_is_enabled;
+    unsigned char _sample_rate[NUM_CHANNELS];
+    unsigned char _frequency[NUM_CHANNELS];
+    unsigned char _pattern[NUM_CHANNELS];
+    unsigned char _pattern_specific[NUM_CHANNELS];
 
-    bool channel_index_out_of_bounds(unsigned int idx);
+    bool channel_index_out_of_bounds(unsigned char idx);
+    bool sample_rate_out_of_bounds(unsigned char sample_rate);
+    bool frequency_out_of_bounds(unsigned char frequency);
+    bool pattern_illegal(unsigned char pattern);
+    bool pattern_specific_out_of_bounds(pattern_t pattern, int pattern_specific);
 
   public:
     command();
-    void set_channel_is_enabled(unsigned int idx);
-    void clr_channel_is_enabled(unsigned int idx);
-    bool get_channel_is_enabled(unsigned int idx);
+
+    void set_channel_is_enabled(unsigned char idx);
+    void clr_channel_is_enabled(unsigned char idx);
+    bool get_channel_is_enabled(unsigned char idx);
+    
+    void set_sample_rate(unsigned char idx, unsigned char sample_rate);
+    unsigned char get_sample_rate(unsigned char idx);
+
+    void set_frequency(unsigned char idx, unsigned char frequency);
+    unsigned char get_frequency(unsigned char idx);
+
+    void set_pattern(unsigned char idx, pattern_t pattern);
+    pattern_t get_pattern(unsigned char idx);
+
+    void set_pattern_specific(unsigned char idx, int _pattern_specific);
+    int get_pattern_specific(unsigned char idx);
+
     ~command();
 };
--- sw/common/src/command.cpp
+++ sw/common/src/command.cpp
@@ -2,16 +2,18 @@
 
 command::command()
 {
-  channel_is_enabled = 0;
+  _channel_is_enabled = 0;
+  for (int ii = 0; ii < NUM_CHANNELS; ii++)
+  {
+    _sample_rate[ii] = 0;
+    _frequency[ii] = 0;
+    _pattern[ii] = (unsigned char)PATTERN_SINE;
+    _pattern_specific[ii] = 0;
+  }
 }
 
-void command::set_channel_is_enabled(unsigned int idx)
-{
-  if (channel_index_out_of_bounds(idx)) { return; }
-  channel_is_enabled |= (unsigned char)(1 << idx);
-}
 
-bool command::channel_index_out_of_bounds(unsigned int idx)
+bool command::channel_index_out_of_bounds(unsigned char idx)
 {
   if (idx < 0 || idx > NUM_CHANNELS)
   {
@@ -21,16 +23,119 @@ bool command::channel_index_out_of_bounds(unsigned int idx)
   return false;
 }
 
-void command::clr_channel_is_enabled(unsigned int idx)
+bool command::sample_rate_out_of_bounds(unsigned char sample_rate)
 {
-  if (channel_index_out_of_bounds(idx)) { return; }
-  channel_is_enabled &= (unsigned char)(~(1 << idx));
+  if (sample_rate > 100000)
+  {
+    cerr << "ERROR! Illegal sample rate " << sample_rate << endl;
+    return true;
+  }
+  return false;
 }
 
-bool command::get_channel_is_enabled(unsigned int idx)
+bool command::frequency_out_of_bounds(unsigned char frequency)
+{
+  if (frequency > frequency / 2)
+  {
+    cerr << "ERROR! Illegal frequency " << frequency << endl;
+    return true;
+  }
+  return false;
+}
+
+bool command::pattern_illegal(unsigned char pattern)
+{
+  if (pattern > NUM_PATTERNS-1)
+  {
+    cerr << "ERROR! Illegal pattern " << pattern << endl;
+    return true;
+  }
+  return false;
+}
+
+bool command::pattern_specific_out_of_bounds(pattern_t pattern, int pattern_specific)
+{
+  if (
+    (pattern == PATTERN_SINE     && (pattern_specific < -3 || pattern_specific > 3))   ||
+    (pattern == PATTERN_SQUARE   && (pattern_specific <  0 || pattern_specific > 100)) ||
+    (pattern == PATTERN_TRIANGLE && (pattern_specific <  0 || pattern_specific > 100)) ||
+    (pattern == PATTERN_SAWTOOTH && (pattern_specific <  0 || pattern_specific > 100))
+  )
+  {
+    cerr << "ERROR! Illegal pattern specific (" << pattern << ") " << pattern_specific << endl;
+    return true;
+  }
+  return false;
+}
+
+void command::set_channel_is_enabled(unsigned char idx)
+{
+  if (channel_index_out_of_bounds(idx)) { return; }
+  _channel_is_enabled |= (1 << idx);
+}
+
+void command::clr_channel_is_enabled(unsigned char idx)
+{
+  if (channel_index_out_of_bounds(idx)) { return; }
+  _channel_is_enabled &= ~(1 << idx);
+}
+
+bool command::get_channel_is_enabled(unsigned char idx)
 {
   if (channel_index_out_of_bounds(idx)) { return false; }
-  return (channel_is_enabled & (unsigned char)(1 << idx));
+  return (_channel_is_enabled & (1 << idx));
+}
+
+void command::set_sample_rate(unsigned char idx, unsigned char sample_rate)
+{
+  if (channel_index_out_of_bounds(idx)) { return; }
+  if (sample_rate_out_of_bounds(sample_rate)) { return; }
+  _sample_rate[idx] = sample_rate;
+}
+
+unsigned char command::get_sample_rate(unsigned char idx)
+{
+  if (channel_index_out_of_bounds(idx)) { return (unsigned char)(-1); }
+  return _sample_rate[idx];
+}
+
+void command::set_frequency(unsigned char idx, unsigned char frequency)
+{
+  if (channel_index_out_of_bounds(idx)) { return; }
+  if (frequency_out_of_bounds(frequency)) { return; }
+  _frequency[idx] = frequency;
+}
+
+unsigned char command::get_frequency(unsigned char idx)
+{
+  if (channel_index_out_of_bounds(idx)) { return (unsigned char)(-1); }
+  return _frequency[idx];
+}
+
+void command::set_pattern(unsigned char idx, pattern_t pattern)
+{
+  if (channel_index_out_of_bounds(idx)) { return; }
+  if (pattern_illegal(pattern)) { return; }
+  _pattern[idx] = pattern;
+}
+
+pattern_t command::get_pattern(unsigned char idx)
+{
+  if (channel_index_out_of_bounds(idx)) { return (pattern_t)(NUM_PATTERNS); }
+  return (pattern_t)_pattern[idx];
+}
+
+void command::set_pattern_specific(unsigned char idx, int pattern_specific)
+{
+  if (channel_index_out_of_bounds(idx)) { return; }
+  if (pattern_specific_out_of_bounds((pattern_t)_pattern[idx], pattern_specific)) { return; }
+  _pattern_specific[idx] = pattern_specific;
+}
+
+int command::get_pattern_specific(unsigned char idx)
+{
+  if (channel_index_out_of_bounds(idx)) { return -1; }
+  return _pattern_specific[idx];
 }
 
 command::~command()
